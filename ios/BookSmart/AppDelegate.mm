292c30e5f807f7b4ba467863092968ac3c0fc68e
Senior Mobile app | Web | API Developer <seniordev52@gmail.com>
2025-03-28 03:36:54 -0400

    add ios permission

--- ios/BookSmart/AppDelegate.mm
+++ ios/BookSmart/AppDelegate.mm
@@ -1,17 +1,65 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>  // Import Firebase
+#import <UserNotifications/UserNotifications.h>  // Import for push notifications
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"BookSmart";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+  // Initialize Firebase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];  // Ensure Firebase is configured
+  }
 
+  // Push Notification Permissions (iOS 10+)
+  if (@available(iOS 10.0, *)) {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound + UNAuthorizationOptionBadge)
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            NSLog(@"Notification permission granted.");
+        } else {
+            NSLog(@"Notification permission denied.");
+        }
+    }];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];  // Register for remote notifications
+  } else {
+    // For iOS < 10
+    UIUserNotificationType allNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:allNotificationTypes
+                                                                               categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+  }
+
+  // Initialize Firebase Messaging (make sure messaging is correctly set)
+  [FIRMessaging messaging].delegate = self;
+
+  self.moduleName = @"BookSmart";
+  self.initialProps = @{};  // Customize initial props if needed
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+// Handle device token registration for Firebase Messaging
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  // Pass the device token to Firebase Messaging
+  [FIRMessaging messaging].APNSToken = deviceToken;
+}
+
+// Handle incoming notifications (foreground and background)
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+    fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  if ([FIRApp defaultApp] != nil) {
+    [[FIRMessaging messaging] appDidReceiveMessage:userInfo];  // Handle Firebase message
+  }
+  completionHandler(UIBackgroundFetchResultNewData);  // Process the background fetch result
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
