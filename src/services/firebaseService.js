56144961e666961848e306deca59773eab5c8158
Senior Mobile app | Web | API Developer <techableteam@gmail.com>
2025-04-01 07:36:47 -0400

    firebase fixing 1

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -4,9 +4,31 @@ import {
   RESULTS,
   requestNotifications,
 } from 'react-native-permissions';
+import { getApp, initializeApp } from '@react-native-firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDe_VKn-VUDe_DBkrFnrwkP5hwcobyHyDY',
+  authDomain: 'booksmartllc-8dd2e.firebaseapp.com',
+  projectId: 'booksmartllc-8dd2e',
+  storageBucket: 'booksmartllc-8dd2e.appspot.com',
+  messagingSenderId: '418587939564',
+  appId: '1:418587939564:ios:d10b5f991088ddca804bc9',
+};
+
+export async function initFirebaseConfig() {
+  let app = null;
+
+  try {
+    app = await getApp();
+  } catch (err) {
+    app = await initializeApp(firebaseConfig);
+  }
+
+  console.log("APP NAME: 111", app.name)
+}
 
 export const requestNotificationsPermission = () => {
-  requestNotifications(['alert']).then(({ status }) => {
+  requestNotifications(['alert', 'sound', 'badge']).then(({ status }) => {
     if (status === RESULTS.GRANTED) {
       console.log("granted!!!!!");
     } else {
@@ -25,8 +47,18 @@ export async function requestUserPermission() {
     console.log('Notification permission granted.');
 
     try {
-      const token = await messaging().getToken();
-      console.log('FCM Token:', token);
+      await messaging().registerDeviceForRemoteMessages();
+      let fcmToken = "";
+      fcmToken = await messaging().getToken();
+      console.log('FCM Token:', fcmToken);
+
+      // ✅ Show Alert with FCM Token for testing
+      Alert.alert(
+        'FCM Token',
+        fcmToken || 'No token received',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
 
       // Handle background notifications
       messaging().setBackgroundMessageHandler(async remoteMessage => {
